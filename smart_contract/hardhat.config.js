86716d33fcbf154a2c1614af4ef594034e357a78
Eui Hyun Kim <hyomithetech@gmail.com>
2022-01-16 23:59:31 -0800

    Error fix: account -> accounts at hardhat.config.js

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,13 +1,11 @@
-
-
-require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-waffle");
 
 module.exports = {
   solidity: '0.8.0',
   networks: {
     ropsten: {
       url: 'https://eth-ropsten.alchemyapi.io/v2/vU43PBkG0gabIwNZv26Q1E0BCVI92Fj_',
-      account: ['b0c82f1e227bc7ff4d647c719f52c59cbf90032d2e500c78cf3c4d466f0ddad3']
+      accounts: ['b0c82f1e227bc7ff4d647c719f52c59cbf90032d2e500c78cf3c4d466f0ddad3']
     }
   }
 }
--- smart_contract/scripts/deploy.js
+++ smart_contract/scripts/deploy.js
@@ -1,15 +1,15 @@
 
 const main = async() => {
   const Transactions = await hre.ethers.getContractFactory("Transactions");
-  const transactions = await Transactions.deploy("Hello, Hardhat!");
+  const transactions = await Transactions.deploy();
 
   await transactions.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("Transactions deployed to:", transactions.address);
 }
 
-const runMain = async() => {
-  try {
+const runMain = async () => {
+  try { 
     await main()
     process.exit(0)
 
